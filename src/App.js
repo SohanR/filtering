8c20aceba025e580d75f66c56daebf265cb99b6f
sohanr <mizan.rahman66d@gmail.com>
2022-01-23 17:08:15 +0600

    static

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      monsters:[
+        {
+          name:"Quinn",
+          id:"aa"
+        },
+        {
+          name:"Harley's Joker",
+          id:"as"
+        }
+      ]
+    };
+  }
+
+
+  
+
+render() {
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  {
+    this.state.monsters.map(monster => <h1 key={monster.id}>{ monster.name }</h1>)
+  }
     </div>
   );
+}
+
 }
 
 export default App;
